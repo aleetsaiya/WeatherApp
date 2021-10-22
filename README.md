003a40cd85affc27752935292c94fb4aaf02d188
aleetsaiya <aleetsaiya@gmail.com>
2021-10-22 09:48:13 +0800

    ranny icon

--- README.md
+++ README.md
@@ -11,11 +11,15 @@
 
 中央氣象局開放資料平臺之資料擷取API: [https://opendata.cwb.gov.tw/dist/opendata-swagger.html](https://opendata.cwb.gov.tw/dist/opendata-swagger.html)
 
+預報XML產品預報因子欄位中文說明表: [https://opendata.cwb.gov.tw/opendatadoc/MFC/D0047.pdf](https://opendata.cwb.gov.tw/opendatadoc/MFC/D0047.pdf)
+
 ## 圖片來源
 Weather Icon Set - Mostly Sunny: [https://dribbble.com/shots/6193524-Weather-Icon-Set-Mostly-Sunny](https://dribbble.com/shots/6193524-Weather-Icon-Set-Mostly-Sunny)
 
+Weather Icon Set - Rain: [https://dribbble.com/shots/6193512-Weather-Icon-Set-Rain](https://dribbble.com/shots/6193512-Weather-Icon-Set-Rain)
+
 ## 心得
-做這次的過程中發現 **"setState 不一定 會馬上更新 `State` 裡面的值"**，如果要立即使用更新完後的 `State` 裡面的值的話，應該使用 updater 來去進行更新。
+做這次的過程中發現 **"setState 不一定 會馬上更新 `State` 裡面的值"**，如果要立即使用更新完後的 `State` 裡面的值的話，應該使用 updater 來去進行更新。還有使用到 `axios` 來進行 api 串接，以及配合 `Promise` 來進行非同步的操作。
 
 [React官方文件-setState()](https://zh-hant.reactjs.org/docs/react-component.html#setstate)
 
--- src/component/WeatherApp.jsx
+++ src/component/WeatherApp.jsx
@@ -2,67 +2,82 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './WeatherApp.css'
 import refresh from '../image/refresh.jpg';
-// https://dribbble.com/shots/6193524-Weather-Icon-Set-Mostly-Sunny
 import sunny from '../image/sunny.gif';
+import ranny from '../image/ranny.gif';
 
 const WeatherApp = () => {
     const authorization = "CWB-52F9DCE3-BDF3-49E6-81A5-32A4AFDC7460";
     const [currentWeather, setCurrentWeather] = useState({
-        locationName: "",
-        temp: "", // 溫度
-        humd: "", // 相對濕度
-        chanceOfRain: "", //降雨機率
-        description: ""
+        locationName: "NaN",
+        temp: "0", // 溫度
+        humd: "0", // 相對濕度
+        chanceOfRain: "0", //降雨機率
+        description: "NaN", // 天氣狀態
+        maxT: "", // 最高溫度
+        minT: "" // 最低溫度
     });
 
     useEffect(() => {
-        fetchCurrentWeather();
-        fetchWeatherPrediction();
+        const fetchData = async () => {
+            const [currentWeather, weatherPrediction] = await Promise.all([
+                fetchCurrentWeather(),
+                fetchWeatherPrediction()
+            ]);
+            if (currentWeather.temp === "NaN") {
+                currentWeather.temp = `${weatherPrediction.minT}~${weatherPrediction.maxT}`;
+            }
+            setCurrentWeather({
+                ...currentWeather,
+                ...weatherPrediction
+            });
+        };
+        
+        fetchData();
     }, []);
 
     const fetchCurrentWeather = () => {
-        console.log('fetch current weather');
         const url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${authorization}`;
-        axios.get(url)
+        return axios.get(url)
             .then(respose => {
                 const locationName = "淡水";
                 const target = respose.data.records.location.find(data => data.locationName === locationName);
-                const temp = parseFloat(target.weatherElement.find(e => e.elementName === "TEMP").elementValue);
-                const humd = parseFloat(target.weatherElement.find(e => e.elementName === "HUMD").elementValue)*100;
-                setCurrentWeather(prevState => {
-                    return {
-                        ...prevState,
-                        locationName,
-                        temp,
-                        humd
-                    }
-                });
-            })
-            .catch(err => {
-                console.log('Error:', err);
+                let temp = parseFloat(target.weatherElement.find(e => e.elementName === "TEMP").elementValue);
+                let humd = parseFloat(target.weatherElement.find(e => e.elementName === "HUMD").elementValue)*100;
+                temp = temp < -50 ? "NaN" : temp;
+                humd = humd < -50 ? "NaN" : humd;
+                return {
+                    locationName,
+                    temp,
+                    humd
+                };
             });
     };
 
     const fetchWeatherPrediction = () => {
-        console.log('fetch weather prediction');
         const url = `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${authorization}`;
-        axios.get(url)
+        return axios.get(url)
             .then(response => {
                 const locationName = "新北市";
                 const data = response.data.records.location.find(obj => obj.locationName === locationName);
                 const description = data.weatherElement.find(e => e.elementName === 'Wx').time[0].parameter.parameterName;
                 const chanceOfRain = data.weatherElement.find(e => e.elementName === 'PoP').time[0].parameter.parameterName;
-                setCurrentWeather(prevState => {
-                    return {
-                        ...prevState,
-                        description,
-                        chanceOfRain
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err);
+                const maxT = data.weatherElement.find(e => e.elementName === 'MaxT').time[0].parameter.parameterName;
+                const minT = data.weatherElement.find(e => e.elementName === 'MinT').time[0].parameter.parameterName;
+                return {
+                    description,
+                    chanceOfRain,
+                    maxT,
+                    minT
+                };
             });
+    }
+
+    const isRanny = () => {
+        const description = currentWeather.description;
+        if (description.includes('雨') || description.includes('陰')) {
+            return true;
+        }
+        return false;
     }
 
     return (
@@ -71,7 +86,7 @@ const WeatherApp = () => {
             <div>{currentWeather.description}</div>
             <div className="main">
                 <div className="temp">{currentWeather.temp}<span>°C</span></div>
-                <img src={sunny} alt="sunny" className="sunny"/>
+                <img src={isRanny ? ranny : sunny} alt="weather icon" className="weatherIcon"/>
             </div>
             <div className="weatherElement">
                 <div>相對溼度: {currentWeather.humd}%</div>
